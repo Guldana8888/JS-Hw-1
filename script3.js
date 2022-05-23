9b129c59c479875ac6908a6f6240380c3e7ab55e
Guldana Turatova <guldana.turatova.kk@gmail.com>
2022-05-24 02:24:03 +0600

    add third script

---
+++ script3.js
@@ -0,0 +1,6 @@
+// Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+let userName = "Guldana";
+   let userEmail = "guldana.turatova.kk@gmail.com";
+    console.log(userName, userEmail);
+    
+document.write("My name is " + userName + "<br>" + "my email: " + userEmail + "<br>" );
